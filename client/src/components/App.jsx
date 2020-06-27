9f7fb2121492ed7c0b764e745c562899bf420aec
Anthony Garitachea <lgaritachea45204@gmail.com>
2020-06-26 22:26:07 -0700

    adds and sets up webpack, babel, and button component for react frontend

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from './Button.jsx';
+
+const App = () => {
+    return(
+        <div>
+            <Button />
+        </div>
+    )
+};
+
+export default App;
---
+++ client/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+    return(
+        <div>
+            <button>Scrape Latest News</button>
+        </div>
+    )
+};
+
+export default Button;
