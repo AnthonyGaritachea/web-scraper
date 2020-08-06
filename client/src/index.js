1439ff1bf09930f12b441f959dd2ec5909670985
Anthony Garitachea <lgaritachea45204@gmail.com>
2020-08-05 22:45:52 -0700

    added redux persist to store data into local storage

--- client/src/index.js
+++ client/src/index.js
@@ -3,21 +3,36 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 import  thunk from 'redux-thunk';
 
-import App from './components/App.jsx'
+import App from './components/App.jsx';
 import rootReducers from './redux/rootReducers.js';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['scrapedNews', 'favoriteStories']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
 const store = createStore(
-    rootReducers,
+    persistedReducer,
     composeWithDevTools(
         applyMiddleware(thunk)
     )
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
